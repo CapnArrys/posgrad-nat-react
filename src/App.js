f5af71866c5928e9f1ac5d1364685211c37c9c1b
CapnArrys <arryshanry@gmail.com>
2025-11-21 19:09:07 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ClienteList from '../src/pages/ClienteList';
+import ClienteForm from '../src/pages/ClienteForm';
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ClienteList />} />
+        <Route path="/clientes" element={<ClienteList />} />
+        <Route path="/clientes/cadastro" element={<ClienteForm />} />
+        <Route path="/clientes/editar/:id" element={<ClienteForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+
+export default App;
